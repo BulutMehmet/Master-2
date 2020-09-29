aed62317c35d380830eb0f11c4e912c582c7673a
BULUT MEHMET <mbulut@localhost.localdomain>
2020-09-29 18:05:12 +0200

    tp1 up to exo 5

---
+++ SyntheseImage/GLImac-Template/TP1/exo3_structure_vertex.cpp
@@ -0,0 +1,99 @@
+#include <glimac/SDLWindowManager.hpp>
+#include <GL/glew.h>
+#include <iostream>
+#include <glimac/Program.hpp>
+#include <glimac/FilePath.hpp>
+#include <glimac/glm.hpp>
+
+using namespace glimac;
+const int VERTEX_ATTR_POSITION = 3;
+const int VERTEX_ATTR_COLOR = 8;
+
+struct Vertex2DColor {
+    glm::vec2 position;
+    glm::vec3 color;
+
+    Vertex2DColor() {
+        position = glm::vec2(0.f, 0.f);
+        color = glm::vec3(0.f, 0.f, 0.f);
+    }
+
+    Vertex2DColor(glm::vec2 pos, glm::vec3 col){
+        position = pos;
+        color = col;
+    }
+};
+
+int main(int argc, char** argv) {
+    // Initialize SDL and open a window
+    SDLWindowManager windowManager(800, 600, "GLImac");
+
+    // Initialize glew for OpenGL3+ support
+    GLenum glewInitError = glewInit();
+    if(GLEW_OK != glewInitError) {
+        std::cerr << glewGetErrorString(glewInitError) << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::cout << "OpenGL Version : " << glGetString(GL_VERSION) << std::endl;
+    std::cout << "GLEW Version : " << glewGetString(GLEW_VERSION) << std::endl;
+
+
+    FilePath applicationPath(argv[0]);
+    Program program = loadProgram(  applicationPath.dirPath() + "shaders/triangle.vs.glsl",
+                                    applicationPath.dirPath() + "shaders/triangle.fs.glsl");
+    program.use();
+    /*********************************
+     * HERE SHOULD COME THE INITIALIZATION CODE
+     *********************************/
+
+    GLuint vbo, vao;
+    Vertex2DColor vertices[] = {  Vertex2DColor(glm::vec2(-0.5f, -0.5f), glm::vec3(1.f, 0.f, 0.f)),
+                            Vertex2DColor(glm::vec2(0.5f, -0.5f), glm::vec3(0.f, 1.f, 0.f)),
+                            Vertex2DColor(glm::vec2(0.0f, 0.5f), glm::vec3(0.f, 0.f, 1.f))
+                            };
+
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, 3 * sizeof(Vertex2DColor), vertices, GL_STATIC_DRAW);
+    glBindBuffer(0, vbo);
+
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glEnableVertexAttribArray(VERTEX_ATTR_POSITION);
+    glEnableVertexAttribArray(VERTEX_ATTR_COLOR);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glVertexAttribPointer(3, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex2DColor), (const GLvoid*) offsetof(Vertex2DColor, position));
+    glVertexAttribPointer(8, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex2DColor), (const GLvoid*) offsetof(Vertex2DColor, color));
+    glBindBuffer(0, vbo);
+    glBindVertexArray(0);
+
+    // Application loop:
+    bool done = false;
+    while(!done) {
+        // Event loop:
+        SDL_Event e;
+        while(windowManager.pollEvent(e)) {
+            if(e.type == SDL_QUIT) {
+                done = true; // Leave the loop after this iteration
+            }
+        }
+
+        /*********************************
+         * HERE SHOULD COME THE RENDERING CODE
+         *********************************/
+
+        glClear(GL_COLOR_BUFFER_BIT);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+
+        // Update the display
+        windowManager.swapBuffers();
+    }
+
+    glDeleteBuffers(1, &vbo);
+    glDeleteVertexArrays(1, &vao);
+
+    return EXIT_SUCCESS;
+}
---
+++ SyntheseImage/build/TP_Template/CMakeFiles/TP_Template_SDLtemplate.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP_Template_SDLtemplate.dir/SDLtemplate.cpp.o"
+  "TP_Template_SDLtemplate.pdb"
+  "TP_Template_SDLtemplate"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TP_Template_SDLtemplate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
